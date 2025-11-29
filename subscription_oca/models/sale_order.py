23f079a2d8bd4fe786bd69e70331b8d2bff0f0ac
Yan Chirino <yanpier.chirino@gmail.com>
2025-11-29 14:13:43 -0500

    [MIG] subscription_oca: Migration to 19.0

--- subscription_oca/models/sale_order.py
+++ subscription_oca/models/sale_order.py
@@ -60,7 +60,8 @@ class SaleOrder(models.Model):
                     "user_id": self.env.context.get("uid", self.env.uid),
                     "template_id": subscription_tmpl.id,
                     "pricelist_id": self.partner_id.property_product_pricelist.id
-                    or self.pricelist_id.id,
+                    if self.partner_id.property_product_pricelist
+                    else self.pricelist_id.id,
                     "date_start": date.today(),
                     "sale_order_id": self.id,
                     "sale_subscription_line_ids": subscription_lines,
